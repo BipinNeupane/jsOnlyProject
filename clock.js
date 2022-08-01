51de4db457a2e76db1b95070be03cf628a64530a
BipinNeupane <bipin2022121@nami.edu.np>
2022-08-01 12:15:10 +0545

    created clock with correct format

--- clock.js
+++ clock.js
@@ -1,7 +1,18 @@
 const myClock = document.getElementById("myClock");
 
 updoot();
-setInterval(updoot,1000)
+// interval to update clock every 1 second
+const myTimre = setInterval(updoot,1000)
+
+// to stop the clock
+document.getElementById("stopClock").onclick = function stopTime(){
+    clearInterval(myTimre); 
+}
+
+// to start the clock again
+document.getElementById("startAgain").onclick = function stopTime(){
+    setInterval(updoot, 1000)
+}
 
 // Function for clock display
 
@@ -15,6 +26,15 @@ function updoot() {
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
 
+        hours = inputZero(hours);
+        minutes = inputZero(minutes);
+        seconds = inputZero(seconds);
+
     return `${hours}:${minutes}:${seconds}`;
   }
+    function inputZero(time){
+         time = time.toString();
+
+         return time.length < 2 ? "0" + time : time;
+    }
 }
